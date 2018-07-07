ebb6363a6e30c8c00da55287db6bce9c889d2f4f
owerbat <niktimak@gmail.com>
2018-07-07 19:15:31 +0300

    First commit

---
+++ Blogger game's test/Blogger game's test/Frame.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger_game_s_test
+{
+    class Frame
+    {
+        private string picture_name;
+        private string blogger_phrase;
+        private int phrases_count; // 1 <= fc <= 4
+        private string[] user_phrases;
+        private Frame[] links;
+        private string voice_name;
+
+        public Frame(string bp = "", int count = 4)
+        {
+            voice_name = picture_name = null;
+            blogger_phrase = bp;
+            phrases_count = count;
+            user_phrases = new string[phrases_count];
+            links = new Frame[phrases_count];
+            for (int i = 0; i < phrases_count; i++)
+            {
+                user_phrases[i] = "";
+                links[i] = null;
+            }
+        }
+
+        public string get_picture()
+        {
+            return picture_name;
+        }
+
+        public void set_picture(string path)
+        {
+            picture_name = path;
+        }
+
+        public string get_blogger_phrase()
+        {
+            return blogger_phrase;
+        }
+
+        public void set_blogger_phrase(string phrase)
+        {
+            blogger_phrase = phrase;
+        }
+
+        public int get_count()
+        {
+            return phrases_count;
+        }
+
+        public void set_count(int count)
+        {
+            phrases_count = count;
+        }
+
+        public string get_phrase(int number)
+        {
+            if (number < 0 || number >= phrases_count)
+                throw new Exception("There is no that link");
+            return user_phrases[number];
+        }
+
+        public void set_phrase(string phrase, int number)
+        {
+            if (number < 0 || number >= phrases_count)
+                throw new Exception("There is no that link");
+            user_phrases[number] = phrase;
+        }
+
+        public Frame get_link(int number)
+        {
+            if (number < 0 || number >= phrases_count)
+                throw new Exception("There is no that link");
+            return links[number];
+        }
+
+        public void set_link(Frame frame, int number)
+        {
+            if (number < 0 || number >= phrases_count)
+                throw new Exception("There is no that link");
+            links[number] = frame;
+        }
+    }
+}
---
+++ Blogger game's test/Blogger game's test/Game.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blogger_game_s_test
+{
+    class Game
+    {
+        public const int FRAME_NUMBER = 30;     //  общее количество количество мест сцен в игре (примерно)
+
+        private Frame first;
+        private Frame current;
+        private Frame[] frames;
+
+        public Game()
+        {
+            frames = new Frame[FRAME_NUMBER];
+            build();
+            first = current = frames[0];
+        }
+
+        public void move(int link_number)
+        {
+            current = current.get_link(link_number);
+        }
+
+        public void build()
+        {
+            frames[0] = new Frame("Привеет!!!", 4);
+            frames[0].set_phrase("Привет!", 0);
+            frames[0].set_phrase("Здарова!", 1);
+            frames[0].set_phrase("Ты кто?", 2);
+            frames[0].set_phrase("Элли, это ты?", 3);
+
+            frames[1] = new Frame("Я Elli Di, блоггер, etc", 1);
+            frames[2] = new Frame("Ты знаешь, кто я?", 2);
+
+            frames[0].set_link(frames[2], 0);
+            frames[0].set_link(frames[2], 1);
+            frames[0].set_link(frames[1], 2);
+            frames[0].set_link(frames[1], 3);    //   frames[0] done
+
+            frames[3] = new Frame("Что бы ты хотела узнать?", 4);
+
+            frames[1].set_phrase("Понятно", 0);
+            frames[1].set_link(frames[3], 0);    //   frames[1] done
+
+            frames[2].set_phrase("Нет", 0);
+            frames[2].set_phrase("Знаю", 1);
+
+            frames[2].set_link(frames[1], 0);
+            frames[2].set_link(frames[3], 1);    //   frames[2] done
+
+
+        }
+    }
+}
